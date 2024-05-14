7b5887683649509b71b854e5d15a0b1eae00c7ac
anarghyacommunication <149464886+anarghyacommunication@users.noreply.github.com>
2024-05-14 16:47:11 +0530

    Add files via upload

---
+++ Pastornotify.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pastornotify = () => {
+
+    return(
+        
+        <div >
+           
+            <marquee><b>Here comes all the Incoming Requests/Chats/Events/Schedules/Reminders will be displayed as an notifications...</b></marquee>
+        </div>
+    )
+};
+export default Pastornotify;
